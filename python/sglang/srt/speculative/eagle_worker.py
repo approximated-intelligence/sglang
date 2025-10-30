cd6dddd30d4c1170f11e1843e4c5938360dc9ceb
yizhang2077 <1109276519@qq.com>
2025-10-30 10:47:09 +0000

    format

--- python/sglang/srt/speculative/eagle_worker.py
+++ python/sglang/srt/speculative/eagle_worker.py
@@ -61,6 +61,7 @@ from sglang.srt.utils import (
     is_cuda,
     next_power_of_2,
 )
+from sglang.srt.utils.common import log_info_on_rank0
 
 if is_cuda():
     from sgl_kernel import segment_packbits  # noqa: F401
@@ -312,7 +313,9 @@ class EAGLEWorker(TpModelWorker):
                 self.mab_last_pull["batch_size"] = batch_size
                 self.mab_last_pull["mab_strategy"] = strategy
                 if self.log_mab_interval % self.server_args.decode_log_interval == 0:
-                    logger.info(f"dynamic tree eagle select strategy = {strategy}")
+                    log_info_on_rank0(
+                        logger, f"dynamic tree eagle select strategy = {strategy}"
+                    )
                 self.log_mab_interval += 1
 
             batch.spec_info.topk_p = batch.spec_info.topk_p[:, : self.topk]
